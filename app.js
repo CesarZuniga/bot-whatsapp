fe95c724c758adf358acf49909f8cc89a429a832
Cesar Zuñiga <cesar.zuniga@mobiik.com>
2023-03-08 10:17:45 -0600

    se deja solo un paso

--- app.js
+++ app.js
@@ -4,62 +4,59 @@ const QRPortalWeb = require('@bot-whatsapp/portal')
 const BaileysProvider = require('@bot-whatsapp/provider/baileys')
 const MockAdapter = require('@bot-whatsapp/database/mock')
 
-const flowSecundario = addKeyword(['2', 'siguiente']).addAnswer(['📄 Aquí tenemos el flujo secundario'])
+// const flowSecundario = addKeyword(['2', 'siguiente']).addAnswer(['📄 Aquí tenemos el flujo secundario'])
 
-const flowDocs = addKeyword(['doc', 'documentacion', 'documentación']).addAnswer(
-    [
-        '📄 Aquí encontras las documentación recuerda que puedes mejorarla',
-        'https://bot-whatsapp.netlify.app/',
-        '\n*2* Para siguiente paso.',
-    ],
-    null,
-    null,
-    [flowSecundario]
-)
+// const flowDocs = addKeyword(['doc', 'documentacion', 'documentación']).addAnswer(
+//     [
+//         '📄 Aquí encontras las documentación recuerda que puedes mejorarla',
+//         'https://bot-whatsapp.netlify.app/',
+//         '\n*2* Para siguiente paso.',
+//     ],
+//     null,
+//     null,
+//     [flowSecundario]
+// )
 
-const flowTuto = addKeyword(['tutorial', 'tuto']).addAnswer(
-    [
-        '🙌 Aquí encontras un ejemplo rapido',
-        'https://bot-whatsapp.netlify.app/docs/example/',
-        '\n*2* Para siguiente paso.',
-    ],
-    null,
-    null,
-    [flowSecundario]
-)
+// const flowTuto = addKeyword(['tutorial', 'tuto']).addAnswer(
+//     [
+//         '🙌 Aquí encontras un ejemplo rapido',
+//         'https://bot-whatsapp.netlify.app/docs/example/',
+//         '\n*2* Para siguiente paso.',
+//     ],
+//     null,
+//     null,
+//     [flowSecundario]
+// )
 
-const flowGracias = addKeyword(['gracias', 'grac']).addAnswer(
-    [
-        '🚀 Puedes aportar tu granito de arena a este proyecto',
-        '[*opencollective*] https://opencollective.com/bot-whatsapp',
-        '[*buymeacoffee*] https://www.buymeacoffee.com/leifermendez',
-        '[*patreon*] https://www.patreon.com/leifermendez',
-        '\n*2* Para siguiente paso.',
-    ],
-    null,
-    null,
-    [flowSecundario]
-)
+// const flowGracias = addKeyword(['gracias', 'grac']).addAnswer(
+//     [
+//         '🚀 Puedes aportar tu granito de arena a este proyecto',
+//         '[*opencollective*] https://opencollective.com/bot-whatsapp',
+//         '[*buymeacoffee*] https://www.buymeacoffee.com/leifermendez',
+//         '[*patreon*] https://www.patreon.com/leifermendez',
+//         '\n*2* Para siguiente paso.',
+//     ],
+//     null,
+//     null,
+//     [flowSecundario]
+// )
 
-const flowDiscord = addKeyword(['discord']).addAnswer(
-    ['🤪 Únete al discord', 'https://link.codigoencasa.com/DISCORD', '\n*2* Para siguiente paso.'],
-    null,
-    null,
-    [flowSecundario]
-)
+// const flowDiscord = addKeyword(['discord']).addAnswer(
+//     ['🤪 Únete al discord', 'https://link.codigoencasa.com/DISCORD', '\n*2* Para siguiente paso.'],
+//     null,
+//     null,
+//     [flowSecundario]
+// )
 
-const flowPrincipal = addKeyword(['hola', 'ole', 'alo'])
-    .addAnswer('🙌 Hola bienvenido a este *Chatbot*')
+const flowPrincipal = addKeyword(['cita', 'citas', 'sita', 'sitas'])
+    .addAnswer('🙌 Hola bienvenid@ a *Beauty&Chic*')
     .addAnswer(
         [
-            'te comparto los siguientes links de interes sobre el proyecto',
-            '👉 *doc* para ver la documentación',
-            '👉 *gracias*  para ver la lista de videos',
-            '👉 *discord* unirte al discord',
-        ],
-        null,
-        null,
-        [flowDocs, flowGracias, flowTuto, flowDiscord]
+            'para checar precios, servicios y agendar Cita',
+            'puedes usar este enlace!!!',
+            '👉 https://book.heygoldie.com/Beauty-Chic  para ver la lista de videos',
+            '👉 *gracias* ',
+        ]
     )
 
 const main = async () => {
